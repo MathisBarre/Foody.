8158e855d94d167a492c54d493e99ab0a4b707c3
Mathis Barré <mathis.barre@live.fr>
2020-04-02 15:46:31 +0200

    end optimize image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,15 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Img from "gatsby-image"
 import BackgroundImage from "gatsby-background-image"
 
-import menuImg from "../images/menu.png"
-import menu1 from "../images/menu1.png"
-import menu2 from "../images/menu2.png"
-import menu3 from "../images/menu3.png"
-import menu4 from "../images/menu4.png"
-import contactImg from "../images/contact.png"
-import { graphql } from "gatsby"
 
-const Home = (props) => {
+const Home = ({image}) => {
   return (
-    <BackgroundImage className="h-32rem lg:h-screen mt-4-8rem lg:mt-0 bg-center bg-cover flex justify-center items-center gradient" fluid={props.image.childImageSharp.fluid} id="home">
+    <BackgroundImage className="h-32rem lg:h-screen mt-4-8rem lg:mt-0 flex justify-center items-center gradient" fluid={image.childImageSharp.fluid} id="home">
       <div className="container mx-auto px-4 z-10 text-white">
         <h2 className="text-2xl md:text-3xl lg:text-6xl xl:text-7xl font-bold mb-2">Making Food Taste Better.</h2>
         <p className="font-serif mb-6 text-lg lg:max-w-50%">Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum, fuit primum ipsius pudore, deinde etiam patris diligentia disciplinaque munita. Qui ut huic virilem togam deditšnihil dicam hoc loco de me; </p>
@@ -25,7 +19,7 @@ const Home = (props) => {
   )
 }
 
-const About = (props) => (
+const About = ({image}) => (
   <section className="container mx-auto px-4 py-6 md:py-12 lg:flex" id="about">
     <div className="inline-block lg:max-w-50%">
       <h2 className="text-2xl md:text-3xl lg:text-6xl xl:text-7xl text-green-600 font-bold">About us</h2>
@@ -34,32 +28,39 @@ const About = (props) => (
       </p>
     </div>
     <div className="hidden lg:block box-border ml-16 my-8 shadow-xl h-auto object-cover max-w-47% w-1/2">
-      <Img fluid={props.image.childImageSharp.fluid} alt="" />
+      <Img fluid={image.childImageSharp.fluid} alt="" />
     </div>
   </section>
 )
 
 const MenuCard = (props) => (
   <div className="h-56 w-48 bg-gray-100 mb-4 md:m-4 flex flex-col justify-start items-center">
-    <div className="h-40 w-40 bg-black mt-4 bg-center bg-cover" style={{backgroundImage: `url(${props.img})`}}></div>
+    <BackgroundImage className="h-40 w-40 bg-black mt-4 bg-center bg-cover" fluid={props.img.childImageSharp.fluid}></BackgroundImage>
     <p className="mt-3">{props.text}</p>
   </div>
 )
 
-const Menu = () => (
-  <section className="bg-center bg-cover" style={{backgroundImage: `url(${menuImg})`}} id="menu">
-    <div className="container mx-auto px-4 py-4 flex justify-center items-center flex-col">
-      <h2 className="text-2xl md:text-3xl lg:text-6xl xl:text-7xl text-green-600 text-center font-bold mt-4">Our Specialities</h2>
-      <p className="text-white text-center lg:max-w-50% mb-8">Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Nulla port or accumsan tincidunt.</p>
-      <div className="flex flex-wrap flex-col md:flex-row justify-center" >
-        <MenuCard text="Italiano - 19.99€" img={menu1} />
-        <MenuCard text="Lemonia - 19.99€" img={menu2} />
-        <MenuCard text="Mangua - 19.99€" img={menu3} />
-        <MenuCard text="Biriyani - 19.99€" img={menu4} />
+const Menu = ({image, images}) => {
+  const titles = [
+    "Italiano - 19.99€",
+    "Lemonia - 19.99€",
+    "Mangua - 19.99€",
+    "Biriyani - 19.99€",
+  ]
+  return (
+    <BackgroundImage className="bg-center bg-cover" fluid={image.childImageSharp.fluid} id="menu">
+      <div className="container mx-auto px-4 py-4 flex justify-center items-center flex-col">
+        <h2 className="text-2xl md:text-3xl lg:text-6xl xl:text-7xl text-green-600 text-center font-bold mt-4">Our Specialities</h2>
+        <p className="text-white text-center lg:max-w-50% mb-8">Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Nulla port or accumsan tincidunt.</p>
+        <div className="flex flex-wrap flex-col md:flex-row justify-center" >
+          {images.edges.map(({ node }, i) => (
+            <MenuCard text={titles[i]} img={node} />
+          ))}
+        </div>
       </div>
-    </div>
-  </section>
-)
+    </BackgroundImage>
+  )
+}
 
 const MealGroup = (props) => (
   <section className="xl:w-47%">
@@ -192,8 +193,8 @@ const FormItem = (props) => (
   </div>
 )
 
-const Contact = () => (
-  <section id="contact" className="bg-center bg-cover" style={{backgroundImage: `url(${contactImg})`}}>
+const Contact = ({ image }) => (
+  <BackgroundImage id="contact" className="bg-center bg-cover" fluid={image.childImageSharp.fluid}>
     <div className="container mx-auto px-4 py-4">
       <h2 className="text-2xl md:text-3xl lg:text-6xl xl:text-7xl text-center font-bold text-green-700 mt-4">Book a table</h2>
       <p className="text-center mb-8 lg:text-lg text-white">Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.</p>
@@ -206,7 +207,7 @@ const Contact = () => (
         <input className="btn text-white mx-auto block mt-8 mb-4" type="submit" value="Submit" />
       </form>
     </div>
-  </section>
+  </BackgroundImage>
 )
 
 
@@ -215,9 +216,9 @@ const IndexPage = ({ data }) => (
     <SEO title="Home" />
     <Home  image={data.home} />
     <About image={data.about} />
-    <Menu />
+    <Menu  image={data.menu} images={data.menus} />
     <Services />
-    <Contact />
+    <Contact image={data.contact} />
   </Layout>
 )
 
@@ -237,7 +238,32 @@ export const query = graphql`
         }
       }
     }
-  }
+    menu: file(relativePath: { eq: "menu.png" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 1920) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    menus: allFile(filter: {relativePath: {regex: "/^menu\\\\d.png$/"}}) {
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+    contact: file(relativePath: { eq: "contact.png" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 1920) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }  
 `
 
 export default IndexPage
